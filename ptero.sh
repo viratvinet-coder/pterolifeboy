463207d09a531c7280c616bb5e910a956d18cf1b
viratvinet-coder <viratvinet@gmail.com>
2025-11-23 13:26:57 +0530

    Update ptero.sh

--- ptero.sh
+++ ptero.sh
@@ -1,25 +1,42 @@
-                                                                                                                                                                      
-                                                                                                                                                                      
-MMMMMMMM               MMMMMMMM                    iiii                        MMMMMMMM               MMMMMMMM                                                        
-M:::::::M             M:::::::M                   i::::i                       M:::::::M             M:::::::M                                                        
-M::::::::M           M::::::::M                    iiii                        M::::::::M           M::::::::M                                                        
-M:::::::::M         M:::::::::M                                                M:::::::::M         M:::::::::M                                                        
-M::::::::::M       M::::::::::M  aaaaaaaaaaaaa   iiiiiiinnnn  nnnnnnnn         M::::::::::M       M::::::::::M    eeeeeeeeeeee    nnnn  nnnnnnnn    uuuuuu    uuuuuu  
-M:::::::::::M     M:::::::::::M  a::::::::::::a  i:::::in:::nn::::::::nn       M:::::::::::M     M:::::::::::M  ee::::::::::::ee  n:::nn::::::::nn  u::::u    u::::u  
-M:::::::M::::M   M::::M:::::::M  aaaaaaaaa:::::a  i::::in::::::::::::::nn      M:::::::M::::M   M::::M:::::::M e::::::eeeee:::::een::::::::::::::nn u::::u    u::::u  
-M::::::M M::::M M::::M M::::::M           a::::a  i::::inn:::::::::::::::n     M::::::M M::::M M::::M M::::::Me::::::e     e:::::enn:::::::::::::::nu::::u    u::::u  
-M::::::M  M::::M::::M  M::::::M    aaaaaaa:::::a  i::::i  n:::::nnnn:::::n     M::::::M  M::::M::::M  M::::::Me:::::::eeeee::::::e  n:::::nnnn:::::nu::::u    u::::u  
-M::::::M   M:::::::M   M::::::M  aa::::::::::::a  i::::i  n::::n    n::::n     M::::::M   M:::::::M   M::::::Me:::::::::::::::::e   n::::n    n::::nu::::u    u::::u  
-M::::::M    M:::::M    M::::::M a::::aaaa::::::a  i::::i  n::::n    n::::n     M::::::M    M:::::M    M::::::Me::::::eeeeeeeeeee    n::::n    n::::nu::::u    u::::u  
-M::::::M     MMMMM     M::::::Ma::::a    a:::::a  i::::i  n::::n    n::::n     M::::::M     MMMMM     M::::::Me:::::::e             n::::n    n::::nu:::::uuuu:::::u  
-M::::::M               M::::::Ma::::a    a:::::a i::::::i n::::n    n::::n     M::::::M               M::::::Me::::::::e            n::::n    n::::nu:::::::::::::::uu
-M::::::M               M::::::Ma:::::aaaa::::::a i::::::i n::::n    n::::n     M::::::M               M::::::M e::::::::eeeeeeee    n::::n    n::::n u:::::::::::::::u
-M::::::M               M::::::M a::::::::::aa:::ai::::::i n::::n    n::::n     M::::::M               M::::::M  ee:::::::::::::e    n::::n    n::::n  uu::::::::uu:::u
-MMMMMMMM               MMMMMMMM  aaaaaaaaaa  aaaaiiiiiiii nnnnnn    nnnnnn     MMMMMMMM               MMMMMMMM    eeeeeeeeeeeeee    nnnnnn    nnnnnn    uuuuuuuu  uuuu
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
+#!/usr/bin/env bash
+
+# Color codes
+CYAN='\e[36m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+RED='\e[31m'
+ENDCOLOR='\e[0m'
+BOLD='\e[1m'
+
+# MAIN MENU banner
+echo -e "${CYAN}${BOLD}"
+echo "  _                                  "
+echo "  /\\/\\   __ _(_)_ __     /\\/\\   ___ _ __  _   _ "
+echo " /    \\ / _\` | | '_ \\   /    \\ / _ \\ '_ \\| | | |"
+echo "/ /\\/\\ \\ (_| | | | | | / /\\/\\ \\  __/ | | | |_| |"
+echo "\\/    \\/\\__,_|_|_| |_| \\/    \\/\\___|_| |_|\\__,_|"
+echo -e "${ENDCOLOR}"
+
+# Creator name
+echo -e "${YELLOW}${BOLD}Made BY LIFEBOYGAMING${ENDCOLOR}"
+
+while true; do
+    echo -e "${GREEN}0) Install Pterodactyl Panel + Wings${ENDCOLOR}"
+    echo -e "${RED}1) Exit${ENDCOLOR}"
+    echo -e "${CYAN}Select an option [0-1]:${ENDCOLOR}"
+    read choice
+
+    case "$choice" in
+        0)
+            echo -e "${GREEN}Running Pterodactyl installation...${ENDCOLOR}"
+            bash <(curl -s https://pterodactyl-installer.se/)
+            ;;
+        1)
+            echo -e "${RED}Exiting.${ENDCOLOR}"
+            exit 0
+            ;;
+        *)
+            echo -e "${RED}Invalid option. Please select 0 or 1.${ENDCOLOR}"
+            ;;
+    esac
+done
